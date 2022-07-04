8a310f1ff431e68e8ffd91604c08cc0c7d84a89b
NematovHojiakbar <hojiakbarnematov0501@gmail.com>
2022-07-04 23:07:40 +0500

    get

---
+++ src/main/java/uz/bsep/controllers/company/CompanyController.java
@@ -0,0 +1,13 @@
+package uz.bsep.controllers.company;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CompanyController {
+    @GetMapping
+    public void getBiId(){
+
+    }
+
+}
--- src/main/java/uz/bsep/controllers/product/ProductController.java
+++ src/main/java/uz/bsep/controllers/product/ProductController.java
@@ -1,19 +1,18 @@
 package uz.bsep.controllers.product;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import uz.bsep.controllers.AbstractController;
 import uz.bsep.dtos.product.ProductCreateDto;
 import uz.bsep.dtos.product.ProductDto;
+import uz.bsep.dtos.product.ProductUpdateDto;
 import uz.bsep.services.product.ProductServiceImpl;
 
 import static uz.bsep.controllers.AbstractController.PATH;
 
-@Controller
+@RestController
 @RequestMapping(PATH+"/product")
 public class ProductController extends AbstractController<ProductServiceImpl> {
     protected ProductController(ProductServiceImpl service) {
@@ -22,19 +21,31 @@ public class ProductController extends AbstractController<ProductServiceImpl> {
 
 
     @PostMapping
-    public String save(ProductCreateDto dto, Model model){
+    public ResponseEntity<?> save(@RequestBody ProductCreateDto dto){
         var id = service.create(dto);
-        model.addAttribute("id", id);
-        return "product";
+        return  ResponseEntity.ok(dto);
     }
 
     @GetMapping("/{id}/{lang}")
-    public String getById(@PathVariable String id, @PathVariable String lang, Model model){
-        var productDto = service.get(id, lang);
-        model.addAttribute("product", productDto);
-        return "product";
+    public ResponseEntity<?> getById(@PathVariable String id, @PathVariable String lang){
+        ProductDto productDto = service.get(id, lang);
+        return ResponseEntity.ok().body(productDto);
     }
 
+
+    @PutMapping("/{id}")
+    public String editById(@PathVariable String id, ProductUpdateDto updateDto,Model model){
+
+        service.update(updateDto);
+        return "";
+    }
+
+
+    @DeleteMapping("/{id}")
+    public String deleteById(@PathVariable String id){
+        service.delete(id);
+        return "";
+    }
 
 
 
